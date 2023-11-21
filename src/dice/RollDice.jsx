e7fdf266de6180d8ca524b8a00f1e5438595488b
Ayannrwz <enriquezadrian.aws@gmail.com>
2023-11-21 13:20:39 +0800

    Die commit

--- src/dice/RollDice.jsx
+++ src/dice/RollDice.jsx
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
-import Die from './Die';
-import '../styles/styles.css';
+import React, { useState } from "react";
+import Die from "./Die";
+import "../styles/styles.css";
+import car from '../dice/car.webp';
 
 const RollDice = () => {
-  const [die1, setDie1] = useState('one');
-  const [die2, setDie2] = useState('one');
+  const [die1, setDie1] = useState("one");
+  const [die2, setDie2] = useState("one");
   const [rolling, setRolling] = useState(false);
 
   const roll = () => {
     setRolling(true);
 
     setTimeout(() => {
-      const faces = ['one', 'two', 'three', 'four', 'five', 'six'];
+      const faces = ["one", "two", "three", "four", "five", "six"];
       setDie1(faces[Math.floor(Math.random() * faces.length)]);
       setDie2(faces[Math.floor(Math.random() * faces.length)]);
       setRolling(false);
@@ -19,9 +20,16 @@ const RollDice = () => {
   };
 
   return (
-    <div>
-      <Die face={die1} rolling={rolling} />
-      <Die face={die2} rolling={rolling} />
+    <div className='die-container'>
+      <div className='car-with-dice'>
+        <img className='car-image' src={car} alt='Car' />
+        <div className='dice-wrapper'>
+          <div className="left-wheel">
+          <Die face={die1} rolling={rolling} /></div>
+          <div className="right-wheel">
+          <Die face={die2} rolling={rolling} /></div>
+        </div>
+      </div>
       <button onClick={roll} disabled={rolling}>
         {rolling ? 'Rolling...' : 'Roll Dice'}
       </button>
